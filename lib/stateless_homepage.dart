55f60f8ca62bd4d2324a8768e968d05d8ee74c63
shinyo17 <17shinyo17@gmail.com>
2023-05-10 16:48:24 +0900

    Initial commit

---
+++ lib/stateless_homepage.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class StatelessHomepage extends StatelessWidget {
+  StatelessHomepage({super.key, required this.title});
+
+  final String title;
+
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _incrementCounter() {
+    _counter++;
+    print(_counter);
+  }
+}
